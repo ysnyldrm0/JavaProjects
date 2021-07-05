b4ab6ff069ede4478b9af58b577f5e65e1afcdda
ysnyldrm0 <ysnyldrm0@hotmail.com>
2021-07-05 17:36:21 +0300

    Enchanced switch fixed

--- Bills Burger v1/src/com/company/Main.java
+++ Bills Burger v1/src/com/company/Main.java
@@ -13,7 +13,7 @@ public class Main {
         addition[0] = new Addition("onion",0.15);
         addition[1] = new Addition("salad",0.4);
         addition[2] = new Addition("ketchup",0.1);
-        addition[3] = new Addition("moyanaisse",0.1);
+        addition[3] = new Addition("mayonnaise",0.1);
         addition[4] = new Addition("tomato",0.15);
         addition[5] = new Addition("potato",0.15);
         addition[6] = new Addition("cola",0.2);
@@ -29,64 +29,69 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         int input;
         int additionalInput;
-        int counter = 0;
+        int counter;
 
-        do {
-            counter = 0;
-            System.out.println(" <-> <-> <-> Welcome to Bills Burger <-> <-> <-> ");
-            System.out.println(" - - - - - - - - - Burger Menu - - - - - - - - - ");
-            System.out.println("1 - " + hamburger.getName() + " - " + hamburger.getPrice() + "$");
-            System.out.println("2 - " + deluxeHamburger.getName() + " - " + deluxeHamburger.getPrice() + "$");
-            System.out.println("3 - " + healthyHamburger.getName() + " - " + healthyHamburger.getPrice() + "$");
-            System.out.println("0 - Cancel Order");
-            System.out.println("Please type your choice: ");
-            input = Integer.parseInt(scanner.nextLine());
+        try {
+            do {
+                counter = 0;
+                System.out.println(" <-> <-> <-> Welcome to Bills Burger <-> <-> <-> ");
+                System.out.println(" - - - - - - - - - Burger Menu - - - - - - - - - ");
+                System.out.println("1 - " + hamburger.getName() + " - " + hamburger.getPrice() + "$");
+                System.out.println("2 - " + deluxeHamburger.getName() + " - " + deluxeHamburger.getPrice() + "$");
+                System.out.println("3 - " + healthyHamburger.getName() + " - " + healthyHamburger.getPrice() + "$");
+                System.out.println("0 - Cancel Order");
+                System.out.println("Please type your choice: ");
+                input = Integer.parseInt(scanner.nextLine());
 
-            switch (input){
-
-                case 1:
-                    System.out.println("For " + hamburger.getName() + " you can add 4 additional brand");
-                    while(counter < 4){
-                        System.out.println((counter+1) + ". addition: ");
-                        additionSelector(addition);
-                        System.out.println("Please type your choice: ");
-                        additionalInput = Integer.parseInt(scanner.nextLine());
-                        if(additionalInput == 0)
-                            counter++;
-                        else {
-                            hamburger.addAddition(addition[additionalInput - 1]);
-                            hamburger.saveAddition(addition[additionalInput - 1]);
-                            counter++;
+                switch (input) {
+                    case 1 -> {
+                        System.out.println("For " + hamburger.getName() + " you can add 4 additional brand");
+                        while (counter < 4) {
+                            System.out.println((counter + 1) + ". addition: ");
+                            additionSelector(addition);
+                            System.out.println("Please type your choice: ");
+                            additionalInput = Integer.parseInt(scanner.nextLine());
+                            if (additionalInput == 0)
+                                counter++;
+                            else {
+                                hamburger.addAddition(addition[additionalInput - 1]);
+                                hamburger.saveAddition(addition[additionalInput - 1]);
+                                counter++;
+                            }
                         }
+                        orderDetails(hamburger);
                     }
-                    orderDetails(hamburger);
-                    break;
-                case 2:
-                    System.out.println("For " + deluxeHamburger.getName() );
-                    orderDetails(deluxeHamburger);
-                    break;
-                case 3:
-                    healthyHamburger.saveAddition(healthyHamburger.getAddition1());
-                    healthyHamburger.saveAddition(healthyHamburger.getAddition2());
-                    System.out.println(healthyHamburger.getName() + " has 2 addition as a default " + healthyHamburger.getAddition1().getName() + " - " + healthyHamburger.getAddition1().getPrice() + "$"
-                            + " & " + healthyHamburger.getAddition2().getName() + " - " + healthyHamburger.getAddition1().getPrice() + "$" + " but if you want you can add 4 additional brand as an extra");
-                    while(counter < 4){
-                        System.out.println((counter+1) + ". addition: ");
-                        additionSelector(addition);
-                        System.out.println("Please type your choice: ");
-                        additionalInput = Integer.parseInt(scanner.nextLine());
-                        if(additionalInput == 0)
-                            counter++;
-                        else {
-                            healthyHamburger.addAddition(addition[additionalInput - 1]);
-                            healthyHamburger.saveAddition(addition[additionalInput - 1]);
-                            counter++;
+                    case 2 -> {
+                        System.out.println("For " + deluxeHamburger.getName());
+                        orderDetails(deluxeHamburger);
+                    }
+                    case 3 -> {
+                        healthyHamburger.saveAddition(healthyHamburger.getAddition1());
+                        healthyHamburger.saveAddition(healthyHamburger.getAddition2());
+                        System.out.println(healthyHamburger.getName() + " has 2 addition as a default " +
+                                healthyHamburger.getAddition1().getName() + " - " + healthyHamburger.getAddition1().getPrice() +
+                                "$" + " & " + healthyHamburger.getAddition2().getName() + " - " + healthyHamburger.getAddition1().getPrice() +
+                                "$" + " but if you want you can add 4 additional brand as an extra");
+                        while (counter < 4) {
+                            System.out.println((counter + 1) + ". addition: ");
+                            additionSelector(addition);
+                            System.out.println("Please type your choice: ");
+                            additionalInput = Integer.parseInt(scanner.nextLine());
+                            if (additionalInput == 0)
+                                counter++;
+                            else {
+                                healthyHamburger.addAddition(addition[additionalInput - 1]);
+                                healthyHamburger.saveAddition(addition[additionalInput - 1]);
+                                counter++;
+                            }
                         }
+                        orderDetails(healthyHamburger);
                     }
-                    orderDetails(healthyHamburger);
-                    break;
-            }
-        }while(input != 0);
+                }
+            }while(input != 0);
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
 
     }
 
