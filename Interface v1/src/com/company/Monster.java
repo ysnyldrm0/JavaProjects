3313e3bd9c1cc47c76b486095e33da3f2b46537b
ysnyldrm0 <ysnyldrm0@hotmail.com>
2021-07-13 16:10:34 +0300

    Interface v1 Added

---
+++ Interface v1/src/com/company/Monster.java
@@ -0,0 +1,57 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Monster implements ISaveable{
+
+    private String name;
+    private int hitPoints;
+    private int strength;
+
+    public Monster(String name, int hitPoints, int strength) {
+        this.name = name;
+        this.hitPoints = hitPoints;
+        this.strength = strength;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHitPoints() {
+        return hitPoints;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+
+    @Override
+    public List<String> write() {
+        List<String> list = new ArrayList<>();
+        list.add(0,name);
+        list.add(1,"" + this.hitPoints);
+        list.add(2,"" + this.strength);
+        return list;
+    }
+
+    @Override
+    public void read(List<String> list) {
+        if( list != null && list.size() > 0){
+            name = list.get(0);
+            hitPoints = Integer.parseInt(list.get(1));
+            strength = Integer.parseInt(list.get(2));
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Monster{" +
+                "name='" + name + '\'' +
+                ", hitPoints=" + hitPoints +
+                ", strength=" + strength +
+                '}';
+    }
+}
---
+++ Interface v1/src/com/company/Player.java
@@ -0,0 +1,82 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Player implements ISaveable{
+
+    private String name;
+    private String weapon;
+    private int hitPoints;
+    private int strength;
+
+    public Player(String name, int hitPoints, int strength) {
+        this.name = name;
+        this.hitPoints = hitPoints;
+        this.strength = strength;
+        this.weapon = "Sword";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getWeapon() {
+        return weapon;
+    }
+
+    public void setWeapon(String weapon) {
+        this.weapon = weapon;
+    }
+
+    public int getHitPoints() {
+        return hitPoints;
+    }
+
+    public void setHitPoints(int hitPoints) {
+        this.hitPoints = hitPoints;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+
+    @Override
+    public List<String> write() {
+        List<String> list = new ArrayList<>();
+        list.add(0,name);
+        list.add(1,"" + this.hitPoints);
+        list.add(2,"" + this.strength);
+        list.add(3,weapon);
+        return list;
+    }
+
+    @Override
+    public void read(List<String> list) {
+        if(list != null && list.size() > 0){
+            name = list.get(0);
+            hitPoints = Integer.parseInt(list.get(1));
+            strength = Integer.parseInt(list.get(2));
+            weapon = list.get(3);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "name='" + name + '\'' +
+                ", hitPoints=" + hitPoints +
+                ", strength=" + strength +
+                ", weapon='" + weapon + '\'' +
+                '}';
+    }
+}
